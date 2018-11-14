6ed82211de6ce2ca2c091a1adf66b434df233171
linfeizhang <753176906@qq.com>
2018-11-14 17:29:19 +0800

    修改告警级别

--- SmartFleet_App_Admin/src/utils/ihtool.js
+++ SmartFleet_App_Admin/src/utils/ihtool.js
@@ -61,13 +61,15 @@ exports.getEventLevelLabel = function getEventLevelLabel(value) {
     if (value == 1) {
         return I18n.t('level1');
     } else if (value == 2) {
-        return '';
+        return I18n.t('level2');
     } else if (value == 3) {
         return I18n.t('level3');
     } else if (value == 4) {
         return I18n.t('level4');
-    } else {
+    } else if (value == 5) {
         return I18n.t('level5');
+    } else {
+        return ''
     }
 }
 
